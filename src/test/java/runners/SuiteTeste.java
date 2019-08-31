1f5a833505c93cf8169311955748ef6958d81057
sergioserejo <sergio_willame3@hotmail.com>
2019-08-31 19:22:50 -0400

    suite

---
+++ src/test/java/runners/SuiteTeste.java
@@ -0,0 +1,13 @@
+package runners;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {Desafio1.class,Desafio2.class})
+//DeletarRatreadorRunner.class,
+//EditarRastreadorRunner.class})
+
+public class SuiteTeste {
+}
